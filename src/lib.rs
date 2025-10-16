03c05ac59367da984d96b1650f97787a1adb3655
SimonIT <simonit.orig@gmail.com>
2025-10-16 18:18:35 +0200

    Try seperate tasks

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,21 @@ mod wg;
 
 pub use crate::config::Config;
 use crate::wg::{MAX_PACKET, consume, create_tunnel, handle_routine_tun_result, send_ip_packet};
+use boringtun::noise::Tunn;
 use boringtun::noise::errors::WireGuardError;
 use core::convert::Infallible;
+use core::future::pending;
 use core::mem::MaybeUninit;
+#[cfg(feature = "defmt")]
+use defmt::{debug, error, info, warn};
+use embassy_executor::Spawner;
 use embassy_futures::select::{Either, select};
 use embassy_net::Stack;
 use embassy_net::udp::{BindError, RecvError, SendError, UdpSocket};
 use embassy_net_driver_channel as ch;
 use embassy_net_driver_channel::driver::LinkState;
-#[cfg(feature = "log")]
-use log::{error, info, warn};
+use embassy_net_driver_channel::{RxRunner, StateRunner, TxRunner};
+use embassy_time::{Duration, Timer};
 use smoltcp::socket::udp::PacketMetadata;
 
 const MTU: usize = 1500;
@@ -69,7 +74,7 @@ pub enum WGError {
     Other,
 }
 
-impl<'d> Runner<'d> {
+impl Runner<'static> {
     /// You must call this in a background task for the driver to operate.
     ///
     /// If reading/writing to the underlying serial port fails, the link state
@@ -84,8 +89,11 @@ impl<'d> Runner<'d> {
         &mut self,
         stack: Stack<'static>,
         config: &Config,
+        spawner: Spawner,
     ) -> Result<Infallible, RunError> {
         stack.wait_config_up().await;
+
+        debug!("Connected to network, starting wireguard");
 
         let (state_chan, mut rx_chan, mut tx_chan) = self.ch.borrow_split();
         state_chan.set_link_state(LinkState::Down);
@@ -95,10 +103,13 @@ impl<'d> Runner<'d> {
         let mut rx_buf = [0; 2048];
         let mut tx_meta = [PacketMetadata::EMPTY; 1];
         let mut tx_buf = [0; 2048];
-        let mut buf = [0; 2048];
+
+        debug!("Creating UDP socket");
 
         let mut socket =
             UdpSocket::new(stack, &mut rx_meta, &mut rx_buf, &mut tx_meta, &mut tx_buf);
+
+        debug!("Bind socket");
 
         if let Err(e) = socket.bind(config.endpoint_bind_addr) {
             // TODO needed?
@@ -107,70 +118,116 @@ impl<'d> Runner<'d> {
             return Err(RunError::Bind(e));
         }
 
+        debug!("Creating tunnel");
+
         let mut tun = create_tunnel(config);
 
-        let mut needs_poll = true;
+        spawner
+            .must_spawn(routine_task(&mut tun, &socket, &config, state_chan));
+        spawner
+            .must_spawn(consume_task(&mut tun, &socket, &config, stack, rx_chan));
+        spawner
+            .must_spawn(produce_task(&mut tun, &socket, &config, tx_chan));
 
+        pending().await
+    }
+}
+
+#[embassy_executor::task]
+async fn routine_task(
+    mut tun: &'static mut Tunn,
+    socket: &'static UdpSocket<'static>,
+    config: &'static Config,
+    state_chan: StateRunner<'static>,
+) {
+    loop {
+        let mut send_buf = [0u8; MAX_PACKET];
+        debug!("Updating timers");
+        let mut res = tun.update_timers(&mut send_buf);
         loop {
-            let mut send_buf = [0u8; MAX_PACKET];
-            let mut res = tun.update_timers(&mut send_buf);
-            loop {
-                match handle_routine_tun_result(&socket, config, res).await {
-                    Ok(_) => {
-                        state_chan.set_link_state(LinkState::Up);
+            debug!("Handling routine result");
+            match handle_routine_tun_result(&socket, config, res).await {
+                Ok(_) => {
+                    debug!("Successfully handled routine result");
+                    state_chan.set_link_state(LinkState::Up);
+                    break;
+                }
+                Err(e) => {
+                    #[cfg(feature = "defmt")]
+                    error!("{:?}", e);
+                    if let WGError::PreparationError(WireGuardError::ConnectionExpired) = e {
+                        #[cfg(feature = "defmt")]
+                        warn!("Wireguard handshake has expired!");
+                        res = tun.format_handshake_initiation(&mut send_buf[..], false);
+                    } else {
+                        state_chan.set_link_state(LinkState::Down);
                         break;
                     }
-                    Err(e) => {
-                        #[cfg(feature = "log")]
-                        error!("{:?}", e);
-                        if let WGError::PreparationError(WireGuardError::ConnectionExpired) = e {
-                            #[cfg(feature = "log")]
-                            warn!("Wireguard handshake has expired!");
-                            res = tun.format_handshake_initiation(&mut send_buf[..], false);
-                        } else {
-                            state_chan.set_link_state(LinkState::Down);
-                            break;
-                        }
-                    }
-                }
-            }
-
-            let rx_fut = async {
-                let rx_buf = rx_chan.rx_buf().await;
-                let rx_data = match needs_poll {
-                    true => &[][..],
-                    false => match socket.recv_from(&mut buf).await {
-                        Ok((0, remote_endpoint)) => return Err(RunError::Eof),
-                        Ok((n, remote_endpoint)) => &buf[..n],
-                        Err(e) => return Err(RunError::Read(e)),
-                    },
-                };
-                Ok((rx_buf, rx_data))
-            };
-            let tx_fut = tx_chan.tx_buf();
-            match select(rx_fut, tx_fut).await {
-                Either::First(r) => {
-                    needs_poll = false;
-                    match consume(stack, &mut tun, &socket, config, r?).await {
-                        Ok(_) => {}
-                        Err(e) => {
-                            #[cfg(feature = "log")]
-                            error!("{:?}", e);
-                        }
-                    }
-                }
-                Either::Second(pkt) => {
-                    match send_ip_packet(&mut tun, &socket, config, pkt).await {
-                        Ok(_) => {}
-                        Err(e) => {
-                            #[cfg(feature = "log")]
-                            error!("{:?}", e);
-                        }
-                    }
-                    tx_chan.tx_done();
                 }
             }
         }
+    }
+}
+
+#[embassy_executor::task]
+async fn consume_task(
+    mut tun: &'static mut Tunn,
+    socket: &'static UdpSocket<'static>,
+    config: &'static Config,
+    stack: Stack<'static>,
+    mut rx_chan: RxRunner<'static, MTU>,
+) {
+    loop {
+        let mut buf = [0; 2048];
+        let rx_buf = rx_chan.rx_buf().await;
+        debug!("Got rx buffer of size {}", rx_buf.len());
+        let rx_data = match socket.recv_from(&mut buf).await {
+            Ok((0, remote_endpoint)) => {
+                error!("Eof");
+                continue;
+            },
+            Ok((n, remote_endpoint)) => &buf[..n],
+            Err(e) => {
+                error!("Failed to read from WireGuard endpoint: {:?}", e);
+                // Sleep a little bit and try again
+                Timer::after(Duration::from_millis(1)).await;
+                continue;
+            },
+        };
+        debug!("Received {} bytes from UDP socket", rx_data.len());
+        debug!("We shall receive a packet");
+        match consume(stack, &mut tun, &socket, config, (rx_buf, rx_data)).await {
+            Ok(_) => {
+                debug!("Consumed packet successfully");
+            }
+            Err(e) => {
+                #[cfg(feature = "defmt")]
+                error!("{:?}", e);
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn produce_task(
+    mut tun: &'static mut Tunn,
+    socket: &'static UdpSocket<'static>,
+    config: &'static Config,
+    mut tx_chan: TxRunner<'static, MTU>,
+) {
+    loop {
+        let tx_fut = tx_chan.tx_buf().await;
+        debug!("We shall send a packet");
+        match send_ip_packet(&mut tun, &socket, config, tx_fut).await {
+            Ok(_) => {
+                debug!("Send packet successfully");
+            }
+            Err(e) => {
+                #[cfg(feature = "defmt")]
+                error!("{:?}", e);
+            }
+        }
+        tx_chan.tx_done();
     }
 }
 
